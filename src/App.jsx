b1bf9ff2d86e97cbdded76afed4c5177737713db
Alex Merced <alex@alexmerced.dev>
2022-10-16 14:42:21 -0400

    version with branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
-import {createSignal, createEffect} from "solid-js"
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import { createSignal, createEffect, batch } from "solid-js";
 
 function App() {
+  const [p1, setP1] = createSignal("Alex Merced");
+  const [p2, setP2] = createSignal("Tony Merced");
 
-  const [p1, setP1] = createSignal("Alex Merced")
-  const [p2, setP2] = createSignal("Tony Merced")
+  let p1Input;
+  let p2Input;
 
-  let p1Input
-  let p2Input
-
-  createEffect(() => console.log(p1(), p2()))
+  createEffect(() => console.log(p1(), p2()));
 
   return (
     <div class={styles.App}>
@@ -18,14 +17,18 @@ function App() {
         <h1>Player: {p1}</h1>
         <h1>Player: {p2}</h1>
       </header>
-      <form onSubmit={(event) => {
-        event.preventDefault()
-        setP1(p1Input.value)
-        setP2(p2Input.value)
-      }}>
-        <input type="text" placeholder="player 1" name="p1" ref={p1Input}/>
-        <input type="text" placeholder="player 2" name="p2" ref={p2Input}/>
-        <input type="submit"/>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          batch(() => {
+            setP1(p1Input.value);
+            setP2(p2Input.value);
+          });
+        }}
+      >
+        <input type="text" placeholder="player 1" name="p1" ref={p1Input} />
+        <input type="text" placeholder="player 2" name="p2" ref={p2Input} />
+        <input type="submit" />
       </form>
     </div>
   );
